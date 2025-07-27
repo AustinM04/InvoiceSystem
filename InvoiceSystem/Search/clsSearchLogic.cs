d8506b71e5977bcb229e0197cf84e12de7284d1d
Austin Moore <austinmoore3@mail.weber.edu>
2025-07-26 21:19:22 -0600

    Search logic implemented

--- InvoiceSystem/Search/clsSearchLogic.cs
+++ InvoiceSystem/Search/clsSearchLogic.cs
@@ -91,17 +91,17 @@ namespace InvoiceSystem.Search
 						// For now, we will return a mock list of invoice numbers.
 						try
 						{
-								//string sSQL = clsSearchSQL.GetAllInvoiceNums();
-								//List<string> nums = new();
-								//int iRetVal = 0;
-								//DataSet ds = new();
-								//ds = ExecuteSQLStatement(sSQL, ref iRetVal);
-								//for (int i = 0; i < iRetVal; i++)
-								//{
-								//		nums.Add(ds.Tables[0].Rows[i]["InvoiceNumber"].ToString());
-								//}
-								//return nums;
-								return new List<string> { "INV001", "INV002", "INV003" };
+								string sSQL = clsSearchSQL.GetAllInvoiceNums();
+								List<string> nums = new();
+								int iRetVal = 0;
+								DataSet ds = new();
+								ds = ExecuteSQLStatement(sSQL, ref iRetVal);
+								for (int i = 0; i < iRetVal; i++)
+								{
+										nums.Add(ds.Tables[0].Rows[i]["InvoiceNum"].ToString());
+								}
+								return nums;
+								//return new List<string> { "INV001", "INV002", "INV003" };
 						}
 						catch (Exception ex)
 						{
@@ -117,17 +117,17 @@ namespace InvoiceSystem.Search
 						// For now, we will return a mock list of invoice dates.
 						try
 						{
-								//string sSQL = clsSearchSQL.GetAllInvoiceDates();
-								//List<string> dates = new();
-								//int iRetVal = 0;
-								//DataSet ds = new();
-								//ds = ExecuteSQLStatement(sSQL, ref iRetVal);
-								//for (int i = 0; i < iRetVal; i++)
-								//{
-								//		dates.Add(ds.Tables[0].Rows[i]["InvoiceDate"].ToString());
-								//}
-								//return dates;
-								return new List<string> { "2023-01-01", "2023-02-01", "2023-03-01" };
+								string sSQL = clsSearchSQL.GetAllInvoiceDates();
+								List<string> dates = new();
+								int iRetVal = 0;
+								DataSet ds = new();
+								ds = ExecuteSQLStatement(sSQL, ref iRetVal);
+								for (int i = 0; i < iRetVal; i++)
+								{
+										dates.Add(ds.Tables[0].Rows[i]["InvoiceDate"].ToString());
+								}
+								return dates;
+								//return new List<string> { "2023-01-01", "2023-02-01", "2023-03-01" };
 						}
 						catch (Exception ex)
 						{
@@ -144,17 +144,17 @@ namespace InvoiceSystem.Search
 						// For now, we will return a mock list of total costs.
 						try
 						{
-								//string sSQL = clsSearchSQL.GetAllInvoiceTotals();
-								//List<string> costs = new();
-								//int iRetVal = 0;
-								//DataSet ds = new();
-								//ds = ExecuteSQLStatement(sSQL, ref iRetVal);
-								//for (int i = 0; i < iRetVal; i++)
-								//{
-								//		costs.Add(ds.Tables[0].Rows[i]["TotalCost"].ToString());
-								//}
-								//return costs;
-								return new List<string> { "100.00", "200.00", "300.00" };
+								string sSQL = clsSearchSQL.GetAllInvoiceTotals();
+								List<string> costs = new();
+								int iRetVal = 0;
+								DataSet ds = new();
+								ds = ExecuteSQLStatement(sSQL, ref iRetVal);
+								for (int i = 0; i < iRetVal; i++)
+								{
+										costs.Add(ds.Tables[0].Rows[i]["TotalCost"].ToString());
+								}
+								return costs;
+								//return new List<string> { "100.00", "200.00", "300.00" };
 						}
 						catch (Exception ex)
 						{
@@ -168,58 +168,53 @@ namespace InvoiceSystem.Search
 
 						try
 						{
-								//string sSQL;
-								//		List<clsInvoice> invoices = new();
-
-								//if (string.IsNullOrEmpty(invoiceNumber) && string.IsNullOrEmpty(invoiceDate) && string.IsNullOrEmpty(totalCost))
-								//{
-
-								//		sSQL = clsSearchSQL.GetAllInvoices();
-								//}
-								//else if(string.IsNullOrEmpty(invoiceNumber) && string.IsNullOrEmpty(invoiceDate) && !string.IsNullOrEmpty(totalCost))
-								//{
-								//		sSQL = clsSearchSQL.GetInvoicesWithCost(totalCost);
-								//}
-								//else if (!string.IsNullOrEmpty(invoiceNumber) && string.IsNullOrEmpty(invoiceDate) && string.IsNullOrEmpty(totalCost))
-								//{
-								//		sSQL = clsSearchSQL.GetInvoiceWithNum(invoiceNumber);
-								//}
-								//else if (!string.IsNullOrEmpty(invoiceNumber) && !string.IsNullOrEmpty(invoiceDate) && string.IsNullOrEmpty(totalCost))
-								//{
-								//		sSQL = clsSearchSQL.GetInvoicesNumDate(invoiceNumber, invoiceDate);
-								//}
-								//else if (!string.IsNullOrEmpty(invoiceNumber) && !string.IsNullOrEmpty(invoiceDate) && !string.IsNullOrEmpty(totalCost))
-								//{
-								//		sSQL = clsSearchSQL.GetInvoicesNumDateCost(invoiceNumber, invoiceDate, totalCost);
-								//}
-								//else if (string.IsNullOrEmpty(invoiceNumber) && !string.IsNullOrEmpty(invoiceDate) && string.IsNullOrEmpty(totalCost))
-								//{
-								//		sSQL = clsSearchSQL.GetInvoicesWithDate(invoiceDate);
-								//}
-								//else if (string.IsNullOrEmpty(invoiceNumber) && !string.IsNullOrEmpty(invoiceDate) && !string.IsNullOrEmpty(totalCost))
-								//{
-								//		sSQL = clsSearchSQL.GetInvoicesCostDate(totalCost, invoiceDate);
-								//}
-								//		int iRetVal = 0;
-								//		DataSet ds = new();
-								//		ds = ExecuteSQLStatement(sSQL, ref iRetVal);
-								//		for (int i = 0; i < iRetVal; i++)
-								//		{
-								//				invoices.Add(new clsInvoice()
-								//				{
-								//						InvoiceNum = ds.Tables[0].Rows[i]["InvoiceNum"].ToString(),
-								//						InvoiceDate = ds.Tables[0].Rows[i]["InvoiceDate"].ToString(),
-								//						TotalCost = ds.Tables[0].Rows[i]["TotalCost"].ToString()
-								//				});
-								//		}
-								//		return invoices;
-								// For now, we will return a mock list of invoices.
-								return new List<clsInvoice>
+								string sSQL = clsSearchSQL.GetAllInvoices();
+								List<clsInvoice> invoices = new();
+						
+								if (string.IsNullOrEmpty(invoiceNumber) && string.IsNullOrEmpty(invoiceDate) && !string.IsNullOrEmpty(totalCost))
 								{
-										new clsInvoice { sInvoiceNum = "1", sInvoiceDate = "2025-01-01", sTotalCost = "100.00" },
-										new clsInvoice { sInvoiceNum = "2", sInvoiceDate = "2025-02-01", sTotalCost = "200.00" },
-										new clsInvoice { sInvoiceNum = "3", sInvoiceDate = "2025-03-01", sTotalCost = "300.00" }
-								};
+										sSQL = clsSearchSQL.GetInvoicesWithCost(totalCost);
+								}
+								else if (!string.IsNullOrEmpty(invoiceNumber) && string.IsNullOrEmpty(invoiceDate) && string.IsNullOrEmpty(totalCost))
+								{
+										sSQL = clsSearchSQL.GetInvoiceWithNum(invoiceNumber);
+								}
+								else if (!string.IsNullOrEmpty(invoiceNumber) && !string.IsNullOrEmpty(invoiceDate) && string.IsNullOrEmpty(totalCost))
+								{
+										sSQL = clsSearchSQL.GetInvoicesNumDate(invoiceNumber, invoiceDate);
+								}
+								else if (!string.IsNullOrEmpty(invoiceNumber) && !string.IsNullOrEmpty(invoiceDate) && !string.IsNullOrEmpty(totalCost))
+								{
+										sSQL = clsSearchSQL.GetInvoicesNumDateCost(invoiceNumber, invoiceDate, totalCost);
+								}
+								else if (string.IsNullOrEmpty(invoiceNumber) && !string.IsNullOrEmpty(invoiceDate) && string.IsNullOrEmpty(totalCost))
+								{
+										sSQL = clsSearchSQL.GetInvoicesWithDate(invoiceDate);
+								}
+								else if (string.IsNullOrEmpty(invoiceNumber) && !string.IsNullOrEmpty(invoiceDate) && !string.IsNullOrEmpty(totalCost))
+								{
+										sSQL = clsSearchSQL.GetInvoicesCostDate(totalCost, invoiceDate);
+								}
+								int iRetVal = 0;
+								DataSet ds = new();
+								ds = ExecuteSQLStatement(sSQL, ref iRetVal);
+								for (int i = 0; i < iRetVal; i++)
+								{
+										invoices.Add(new clsInvoice()
+										{
+												sInvoiceNum = ds.Tables[0].Rows[i]["InvoiceNum"].ToString(),
+												sInvoiceDate = ds.Tables[0].Rows[i]["InvoiceDate"].ToString(),
+												sTotalCost = ds.Tables[0].Rows[i]["TotalCost"].ToString()
+										});
+								}
+								return invoices;
+								// For now, we will return a mock list of invoices.
+								//return new List<clsInvoice>
+								//{
+								//		new clsInvoice { sInvoiceNum = "1", sInvoiceDate = "2025-01-01", sTotalCost = "100.00" },
+								//		new clsInvoice { sInvoiceNum = "2", sInvoiceDate = "2025-02-01", sTotalCost = "200.00" },
+								//		new clsInvoice { sInvoiceNum = "3", sInvoiceDate = "2025-03-01", sTotalCost = "300.00" }
+								//};
 						}
 						catch (Exception ex)
 						{
