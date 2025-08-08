e8cafd2dff00632f30b61ca2fd16db6fddf7a531
ANG4 <36510355+ANG4@users.noreply.github.com>
2025-08-08 14:42:54 -0600

    Update clsItem.cs
    Added a Constructor and a Override ToString Method.

--- InvoiceSystem/Common/clsItem.cs
+++ InvoiceSystem/Common/clsItem.cs
@@ -6,19 +6,53 @@ using System.Threading.Tasks;
 
 namespace InvoiceSystem.Common
 {
-		internal class clsItem
-		{
-				/// <summary>
-				/// Item Code
-				/// </summary>
-				public string sItemCode { get; set; }
-				/// <summary>
-				/// Item Description
-				/// </summary>
-				public string sDescription { get; set; }
-				/// <summary>
-				/// Item Cost
-				/// </summary>
-				public string sCost { get; set; }
-		}
+	internal class clsItem
+	{
+		/// <summary>
+		/// Item Code
+		/// </summary>
+		public string sItemCode { get; set; }
+		/// <summary>
+		/// Item Description
+		/// </summary>
+		public string sDescription { get; set; }
+		/// <summary>
+		/// Item Cost
+		/// </summary>
+		public string sCost { get; set; }
+		/// <summary>
+        /// Constructor
+        /// </summary>
+        public clsItem(string sitemDesc, string sitemCode, string sitemCost) 
+        {
+            try
+            {
+                sDescription = sitemDesc;
+                sItemCode = sitemCode;
+                sCost = sitemCost;
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(MethodInfo.GetCurrentMethod().DeclaringType.Name + 
+                    "." + MethodInfo.GetCurrentMethod().Name + " -> " + ex.Message);
+            }
+        }
+		///<summary>
+		///Override ToString Method
+		///</summary>
+		public override string ToString()
+        {
+            try
+            {
+                return sDescription;
+            }
+            catch (Exception ex)
+            {
+                //Just throw the exception
+                throw new Exception(MethodInfo.GetCurrentMethod().DeclaringType.Name + "." +
+                                    MethodInfo.GetCurrentMethod().Name + " -> " + 
+                                    ex.Message);
+            }
+        }
+	}
 }
