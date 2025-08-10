1d887ef8c6efd3e297a572685e7eb53f1b3d990c
ANG4 <36510355+ANG4@users.noreply.github.com>
2025-08-09 20:46:53 -0600

    Redone Entire Code

--- InvoiceSystem/Items/clsItemsSQL.cs
+++ InvoiceSystem/Items/clsItemsSQL.cs
@@ -10,92 +10,121 @@ namespace InvoiceSystem.Items
     internal class clsItemsSQL
     {
         /// <summary>
-        /// This SQL gets all item details
+        /// Return all items in the table. 
         /// </summary>
-        /// <returns>SQL Select Statement as string</returns>
-        public static string GetAllItemDetails()
+        /// <returns>SQL statement</returns>
+        public string getAllItems()
         {
             try
             {
-                return "SELECT ItemCode, ItemDesc, Cost from ItemDesc";
+                //Gets Items by ItemCode, Description, and Cost
+                return "select ItemCode, ItemDesc, Cost from ItemDesc order by ItemDesc";
             }
             catch (Exception ex)
             {
-                throw new Exception(MethodInfo.GetCurrentMethod().DeclaringType.Name + "." +
-                    MethodInfo.GetCurrentMethod().Name + " -> " + ex.Message);
+                //Throws Exception
+                throw new Exception(ExceptionChain(MethodInfo.GetCurrentMethod(), ex));
             }
         }
 
         /// <summary>
-        /// This SQL gets all the invoices that contain a specific item code.
+        /// Returns a distinct (no duplicates) list of invoices numbers associated with an itemCode
         /// </summary>
-        /// <param name="itemCode">The item code to retrieve all invoices for.</param>
-        /// <returns>All invoices for the given item code.</returns>
-        public static string GetItemWithCode(string itemCode)
+        /// <param name="itemCode">Unique identifier for an item description.</param>
+        /// <returns>SQL statement</returns>
+        public string getAllInvoicesForItemCode(string itemCode)
         {
             try
             {
-                return $"SELECT distinct(InvoiceNum) from LineItems where ItemCode = '{itemCode}'";
+                return String.Format("select distinct(InvoiceNum) from LineItems where ItemCode = '{0}'", itemCode);
             }
             catch (Exception ex)
             {
-                throw new Exception(MethodInfo.GetCurrentMethod().DeclaringType.Name + "." +
-                    MethodInfo.GetCurrentMethod().Name + " -> " + ex.Message);
+                throw new Exception(ExceptionChain(MethodInfo.GetCurrentMethod(), ex));
             }
         }
 
         /// <summary>
-        /// SQL to update existing item
+        /// Get the max id from the items table
         /// </summary>
-        public static string UpdateItem(string itemDesc, string itemCost, string itemCode)
-        {
-            try
-            {
-                return $"Update ItemDesc Set ItemDesc = '{itemDesc}', Cost = {itemCost} where ItemCode = '{itemCode}'";
-            }
-            catch (Exception ex)
-            {
-                throw new Exception(MethodInfo.GetCurrentMethod().DeclaringType.Name + "." +
-                    MethodInfo.GetCurrentMethod().Name + " -> " + ex.Message);
-            }
-        }
-
-        /// <summary>
-        /// This SQL inserts a new item into the ItemDesc table
-        /// </summary>
-        /// <param name="itemCode">Item code for the new item</param>
-        /// <param name="itemDesc">Description for the new item</param>
-        /// <param name="itemPrice">Price for the new item</param>
         /// <returns></returns>
-        public static string InsertItem(string itemCode, string itemDesc, string itemPrice)
+        public string getMaxIdFromItems()
         {
             try
             {
-                return $"Insert into ItemDesc(ItemCode, ItemDesc, Cost) Values('{itemCode}', '{itemDesc}', {itemPrice});";
+                return "select max(ItemCode) from ItemDesc";
             }
             catch (Exception ex)
             {
-                throw new Exception(MethodInfo.GetCurrentMethod().DeclaringType.Name + "." +
-                    MethodInfo.GetCurrentMethod().Name + " -> " + ex.Message);
+                throw new Exception(ExceptionChain(MethodInfo.GetCurrentMethod(), ex));
             }
         }
 
         /// <summary>
-        /// This SQL deletes an item from the ItemDesc table based on the item code
+        /// Updates an existing item description.
         /// </summary>
-        /// <param name="itemCode"></param>
-        /// <returns></returns>
-        public static string DeleteItem(string itemCode)
+        /// <param name="desc">Description of item</param>
+        /// <param name="cost">Cost of item in decimal</param>
+        /// <param name="itemCode">Unique identifier for an item description.</param>
+        /// <returns>SQL statement</returns>
+        public string updateItemDescription(string desc, decimal cost, string itemCode)
         {
             try
             {
-                return $"Delete from ItemDesc Where ItemCode = '{itemCode}'";
+                return String.Format("Update ItemDesc Set ItemDesc = '{0}', Cost = {1} where ItemCode = '{2}'", desc, cost, itemCode);
             }
             catch (Exception ex)
             {
-                throw new Exception(MethodInfo.GetCurrentMethod().DeclaringType.Name + "." +
-                    MethodInfo.GetCurrentMethod().Name + " -> " + ex.Message);
+                throw new Exception(ExceptionChain(MethodInfo.GetCurrentMethod(), ex));
             }
+        }
+
+        /// <summary>
+        /// Create a new item description. 
+        /// </summary>
+        /// <param name="itemCode">Unique identifier for an item description.</param>
+        /// <param name="desc">Description of item</param>
+        /// <param name="cost">Cost of item in decimal</param>
+        /// <returns>SQL statement</returns>
+        public string addNewItemDescription(string itemCode, string desc, decimal cost)
+        {
+            try
+            {
+                return String.Format("Insert into ItemDesc (ItemCode, ItemDesc, Cost) Values ('{0}', '{1}', {2})", itemCode, desc, cost);
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ExceptionChain(MethodInfo.GetCurrentMethod(), ex));
+            }
+        }
+
+        /// <summary>
+        /// Remove an existing item description
+        /// </summary>
+        /// <param name="itemCode">Unique identifier for an item description.</param>
+        /// <returns>SQL statement</returns>
+        public string deleteItemDescription(string itemCode)
+        {
+            try
+            {
+                return String.Format("Delete from ItemDesc Where ItemCode = '{0}'", itemCode);
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ExceptionChain(MethodInfo.GetCurrentMethod(), ex));
+            }
+        }
+
+        /// <summary>
+        /// Returns error info as a string in a consistent format to make debugging easier
+        /// </summary>
+        /// <param name="mb"></param>
+        /// <param name="ex"></param>
+        /// <returns></returns>
+        private static string ExceptionChain(MethodBase mb, Exception ex)
+        {
+            return string.Format("{0}.{1}->{2}", mb.DeclaringType.Name, mb.Name, ex.Message);
         }
     }
 }
+
